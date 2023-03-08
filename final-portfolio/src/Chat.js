5ed3c2c45e091809ea41c88a0013cc7e9f9b385b
James C <james.sy.coward@gmail.com>
2023-03-08 01:08:23 +0000

    yep yep yep

---
+++ final-portfolio/src/Chat.js
@@ -0,0 +1,8 @@
+export default function Chat() {
+    return (
+        <iframe src="https://www5.cbox.ws/box/?boxid=941837&boxtag=T1Tql7" width="100%" height="450" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto">
+         </iframe>	
+                    
+
+    );
+}
